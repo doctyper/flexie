7b4e0859d40548a3568addf2bfb005b1ca223c5c
Richard Herrera <rich@doctyper.com>
2010-10-17 16:09:48 -0700

    Initial commit.

---
+++ js/flexie.js
@@ -0,0 +1,30 @@
+/*
+File: flexie.js
+
+About: Version
+	0.1 pre
+
+Project: Flexie
+
+Description:
+	Legacy support for the CSS3 Flexible Box Model
+
+*/
+
+/*
+Class: Flexie
+	Scoped to the Flexie Global Namespace
+*/
+var Flexie = window.Flexie || (function() {
+	var $self = this;
+	
+	$self.vars = {};
+	
+	$self.utils = {};
+	
+	$self.init = function() {
+		
+	}();
+	
+	return $self;
+})();
