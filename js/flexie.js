1a7c0139016d35f6a58fa2c770421597b6b2d6a4
Richard Herrera <rich@doctyper.com>
2010-10-18 09:10:38 -0700

    Shelving the CSS-sniffing for now. Too complicated. Maybe I can piggyback on some other parse engine later.

--- js/flexie.js
+++ js/flexie.js
@@ -18,13 +18,42 @@ Class: Flexie
 var Flexie = window.Flexie || (function() {
 	var $self = this;
 	
-	$self.vars = {};
+	$self.vars = {
+		prefixes : " -o- -moz- -ms- -webkit- -khtml- ".split(" ")
+	};
 	
-	$self.utils = {};
-	
-	$self.init = function() {
+	$self.utils = {
+		appendProperty : function(target, prop, value) {
+			var prefixes = $self.vars.prefixes;
+			
+			for (var i = 0, j = prefixes.length; i < j; i++) {
+				target.style[prop] = prefixes[i] + value;
+			}
+			
+			target.style[prop] = value;
+			return target;
+		},
 		
-	}();
+		flexBoxSupported : function() {
+			var dummy = document.createElement("div"),
+			    display = "box";
+			
+			this.appendProperty(dummy, "display", "box");
+			
+			document.body.appendChild(dummy);
+			return dummy.style.display ? true : false;
+		}
+	};
+	
+	$self.box = function(params) {
+		// console.log(params);
+		// console.log(window.getComputedStyle(params.target, null).getPropertyValue("display"));
+		var support = $self.utils.flexBoxSupported();
+		
+		if (!support) {
+			console.log(support);
+		}
+	};
 	
 	return $self;
 })();
