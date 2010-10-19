797ca912339604da503e868f966321913d70900a
Richard Herrera <rich@doctyper.com>
2010-10-19 13:36:51 -0700

    - Using Selectivizr's <http://selectivizr.com/> CSS parsing engine to auto-detect Flex Box Elements. No need for a separate Flexie call :)
    - Currently it only matches the Flex Box parent. Will need to build support for Flex Box Children.
    
    - Some issues around how to scope function. Will need to address in a later build.

--- js/flexie.js
+++ js/flexie.js
@@ -22,6 +22,298 @@ var Flexie = window.Flexie || (function() {
 		prefixes : " -o- -moz- -ms- -webkit- -khtml- ".split(" ")
 	};
 	
+	/*
+	selectivizr v1.0.0 - (c) Keith Clark, freely distributable under the terms 
+	of the MIT license.
+
+	selectivizr.com
+	*/
+	$self.selectivizr = {
+		RE_COMMENT : /(\/\*[^*]*\*+([^\/][^*]*\*+)*\/)\s*/g,
+		
+		RE_SELECTOR_GROUP : /(^|})\s*([^\{]*?[\[:][^{]+)/g,
+		
+		// Whitespace normalization regexp's
+		RE_TIDY_TRAILING_WHITESPACE : /([(\[+~])\s+/g,
+		RE_TIDY_LEADING_WHITESPACE : /\s+([)\]+~])/g,
+		RE_TIDY_CONSECUTIVE_WHITESPACE : /\s+/g,
+		RE_TIDY_TRIM_WHITESPACE : /^\s*((?:[\S\s]*\S)?)\s*$/,
+		
+		// String constants
+		EMPTY_STRING : "",
+		SPACE_STRING : " ",
+		PLACEHOLDER_STRING : "$1",
+
+		// --[ patchStyleSheet() ]----------------------------------------------
+		// Scans the passed cssText for selectors that require emulation and
+		// creates one or more patches for each matched selector.
+		patchStyleSheet : function(cssText) {
+			var _self = this;
+			
+			return cssText.replace(_self.RE_SELECTOR_GROUP, function(m, prefix, selectorText) {	
+				var selectorGroups = selectorText.split(",");
+				for (var c = 0, cs = selectorGroups.length; c < cs; c++) {
+					var selector = _self.normalizeSelectorWhitespace(selectorGroups[c]) + _self.SPACE_STRING;
+					var patches = [];
+				}
+				return prefix + selectorGroups.join(",");
+			});
+		},
+
+		// --[ trim() ]---------------------------------------------------------
+		// removes leading, trailing whitespace from a string
+		trim : function(text) {
+			return text.replace(this.RE_TIDY_TRIM_WHITESPACE, this.PLACEHOLDER_STRING);
+		},
+
+		// --[ normalizeWhitespace() ]------------------------------------------
+		// removes leading, trailing and consecutive whitespace from a string
+		normalizeWhitespace : function(text) {
+			return this.trim(text).replace(this.RE_TIDY_CONSECUTIVE_WHITESPACE, this.SPACE_STRING);
+		},
+
+		// --[ normalizeSelectorWhitespace() ]----------------------------------
+		// tidys whitespace around selector brackets and combinators
+		normalizeSelectorWhitespace : function(selectorText) {
+			return this.normalizeWhitespace(selectorText.replace(this.RE_TIDY_TRAILING_WHITESPACE, this.PLACEHOLDER_STRING).replace(this.RE_TIDY_LEADING_WHITESPACE, this.PLACEHOLDER_STRING));
+		},
+		
+		// --[ determineSelectorMethod() ]--------------------------------------
+		// walks through the selectorEngines object testing for an suitable
+		// selector engine.
+		determineSelectorMethod : function() {
+			// compatiable selector engines in order of CSS3 support
+			var selectorEngines = {
+				"NW" : "*.Dom.select",
+				"DOMAssistant" : "*.$", 
+				"Prototype" : "$$",
+				"YAHOO" : "*.util.Selector.query",
+				"MooTools" : "$$",
+				"Sizzle" : "*", 
+				"jQuery" : "*",
+				"dojo" : "*.query"
+			};
+			
+			var win = window, method;
+			
+			for (var engine in selectorEngines) {
+				if (win[engine] && (method = eval(selectorEngines[engine].replace("*",engine)))) {
+					return method;
+				}
+			}
+			return false;
+		},
+		
+		// --[ getXHRObject() ]-------------------------------------------------
+		getXHRObject : function() {
+			if (window.XMLHttpRequest) {
+				return new XMLHttpRequest;
+			}
+			
+			try	{ 
+				return new ActiveXObject('Microsoft.XMLHTTP') ;
+			} catch(e) { 
+				return null;
+			}
+		},
+		
+		// --[ loadStyleSheet() ]-----------------------------------------------
+		loadStyleSheet : function(url) {
+			var xhr = this.getXHRObject();
+			
+			xhr.open("GET", url, false);
+			xhr.send();
+			return (xhr.status === 200) ? xhr.responseText : "";	
+		},
+		
+		// --[ resolveUrl() ]---------------------------------------------------
+		// Converts a URL fragment to a fully qualified URL using the specified
+		// context URL. Returns null if same-origin policy is broken
+		resolveUrl : function(url, contextUrl) {
+			function getProtocolAndHost(url) {
+				return url.substring(0, url.indexOf("/", 8));
+			}
+
+			// absolute path
+			if (/^https?:\/\//i.test(url)) {
+				return getProtocolAndHost(contextUrl) == getProtocolAndHost(url) ? url : null;
+			}
+
+			// root-relative path
+			if (url.charAt(0) == "/")	{
+				return getProtocolAndHost(contextUrl) + url;
+			}
+
+			// relative path
+			var contextUrlPath = contextUrl.split("?")[0]; // ignore query string in the contextUrl
+			if (url.charAt(0) != "?" && contextUrlPath.charAt(contextUrlPath.length-1) != "/") {
+				contextUrlPath = contextUrlPath.substring(0, contextUrlPath.lastIndexOf("/") + 1);
+			}
+
+			return contextUrlPath + url;
+		},
+		
+		// --[ parseStyleSheet() ]----------------------------------------------
+		// Downloads the stylesheet specified by the URL, removes it's comments
+		// and recursivly replaces @import rules with their contents, ultimately
+		// returning the full cssText.
+		parseStyleSheet : function(url) {
+			var _self = this;
+			
+			if (url) {
+				var cssText = this.loadStyleSheet(url);
+				return cssText.replace(_self.RE_COMMENT, _self.EMPTY_STRING).replace(_self.RE_IMPORT, function( match, quoteChar, importUrl ) { 
+					return _self.parseStyleSheet(_self.resolveUrl(importUrl, url));
+				});
+			}
+			return _self.EMPTY_STRING;
+		},
+		
+		// --[ init() ]---------------------------------------------------------
+		init : function() {
+			// honour the <base> tag
+			var doc = document, url, stylesheet, c,
+			    baseTags = doc.getElementsByTagName("BASE"),
+			    baseUrl = (baseTags.length > 0) ? baseTags[0].href : doc.location.href;
+			
+			for (c = 0; c < doc.styleSheets.length; c++) {
+				stylesheet = doc.styleSheets[c];
+				
+				if (stylesheet.href != "") {
+					url = this.resolveUrl(stylesheet.href, baseUrl);
+					
+					if (url) {
+						var cssText = this.patchStyleSheet(this.parseStyleSheet(url)),
+						    tree = this.buildSelectorTree(cssText),
+						    flexers = this.findFlexBoxElements(tree);
+					}
+				}
+			}
+			
+			this.buildFlexieCall(flexers);
+		},
+		
+		buildSelectorTree : function(text) {
+			var rules = [], ruletext, i, j, k, l, rule,
+			    match, selector, proptext, splitprop, properties;
+			
+			// Tabs, Returns
+			text = text.replace(/\t/g, "").replace(/\n/g, "").replace(/\r/g, "");
+			
+			// Leading / Trailing Whitespace
+			text = text.replace(/\s?(\{|\:|\})\s?/g, "$1");
+			
+			ruletext = text.split("}");
+			
+			for (i = 0, j = ruletext.length; i < j; i++) {
+				if (ruletext[i]) {
+					rule = [ruletext[i], "}"].join("");
+					
+					match = (/(.*)\{(.*)\}/).exec(rule);
+					
+					if (match.length && match[2]) {
+						selector = match[1];
+						proptext = match[2].split(";");
+						properties = [];
+						
+						for (k = 0, l = proptext.length; k < l; k++) {
+							splitprop = proptext[k].split(":");
+							
+							if (splitprop.length && splitprop[1]) {
+								properties.push({
+									property : splitprop[0],
+									value : splitprop[1]
+								});
+							}
+						}
+						
+						rules.push({
+							selector : selector,
+							properties : properties
+						});
+					}
+				}
+			}
+			
+			return rules;
+		},
+		
+		findFlexBoxElements : function(rules) {
+			this.flexers = this.flexers || [];
+			
+			var flexers = this.flexers, i, j, k, l,
+			    rule, selector, properties, prop,
+			    property, value;
+			
+			for (i = 0, j = rules.length; i < j; i++) {
+				rule = rules[i];
+				selector = rule.selector;
+				properties = rule.properties;
+				
+				for (k = 0, l = properties.length; k < l; k++) {
+					prop = properties[k];
+					property = prop.property;
+					value = prop.value;
+					
+					if (property == "display" && value == "box") {
+						flexers.push(rule);
+					}
+				}
+			}
+			
+			return flexers;
+		},
+		
+		buildFlexieCall : function(flexers) {
+			var i, j, k, l, flex, selector, properties, prop,
+			    orient, align, direction, pack, lib, caller;
+			
+			for (i = 0, j = flexers.length; i < j; i++) {
+				flex = flexers[i];
+				
+				selector = flex.selector;
+				properties = flex.properties;
+				
+				orient = align = direction = pack = null;
+				
+				for (k = 0, l = properties.length; k < l; k++) {
+					prop = properties[k];
+					
+					switch (prop.property) {
+						case "box-orient" :
+						orient = prop.value;
+						break;
+						
+						case "box-align" :
+						align = prop.value;
+						break;
+						
+						case "box-direction" :
+						direction = prop.value;
+						break;
+						
+						case "box-pack" :
+						pack = prop.value;
+						break;
+					}
+				}
+				
+				if (orient || align || direction || pack) {
+					lib = this.determineSelectorMethod();
+					caller = lib(flex.selector);
+					
+					new $self.box({
+						"target" : caller[0] || caller,
+						"box-orient" : orient,
+						"box-align" : align,
+						"box-direction" : direction,
+						"box-pack" : pack
+					});
+				}
+			}
+		}
+	};
+	
 	$self.utils = {
 		appendProperty : function(target, prop, value) {
 			var prefixes = $self.vars.prefixes,
@@ -164,5 +456,11 @@ var Flexie = window.Flexie || (function() {
 		}
 	};
 	
+	$self.init = function() {
+		$self.selectivizr.init();
+	};
+	
 	return $self;
 })();
+
+Flexie.init();
