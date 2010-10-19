916ec40c1436ef7fd49650b9b81623478ca41801
Richard Herrera <rich@doctyper.com>
2010-10-18 18:11:04 -0700

    Starting with basic box-orient and box-align support. Perhaps inline-block will be of assistance.

--- js/flexie.js
+++ js/flexie.js
@@ -24,34 +24,107 @@ var Flexie = window.Flexie || (function() {
 	
 	$self.utils = {
 		appendProperty : function(target, prop, value) {
-			var prefixes = $self.vars.prefixes;
+			var prefixes = $self.vars.prefixes,
+			    cssText = [];
 			
 			for (var i = 0, j = prefixes.length; i < j; i++) {
-				target.style[prop] = prefixes[i] + value;
+				cssText.push(prop + ":" + prefixes[i] + value);
 			}
 			
-			target.style[prop] = value;
+			target.style.cssText = cssText.join(";");
 			return target;
 		},
 		
+		applyBoxModel : function(target, children) {
+			target.style.overflow = "hidden";
+		},
+		
+		applyBoxOrient : function(target, children, orient) {
+			switch (orient) {
+				case "horizontal" :
+				case "inline-axis" :
+					
+					for (var i = 0, j = children.length; i < j; i++) {
+						var kid = children[i];
+						kid.style.cssFloat = "left";
+						kid.style.styleFloat = "left";
+					}
+
+				break;
+				
+				case "vertical" :
+				case "block-axis":
+				break;
+			}
+		},
+		
+		applyBoxAlign : function(target, children, align) {
+			switch (align) {
+				case "stretch" :
+					
+					for (var i = 0, j = children.length; i < j; i++) {
+						var kid = children[i];
+						kid.style.height = target.offsetHeight + "px";
+					}
+
+				break;
+				
+				case "start" :
+				break;
+				
+				case "end" :
+				break;
+				
+				case "center":
+				break;
+				
+				case "baseline":
+				break;
+			}
+		},
+		
+		applyBoxDirection : function(target, children, direction) {
+			
+		},
+		
+		applyBoxPack : function(target, children, pack) {
+			
+		},
+		
 		flexBoxSupported : function() {
-			var dummy = document.createElement("div"),
-			    display = "box";
-			
+			var dummy = document.createElement("div");
 			this.appendProperty(dummy, "display", "box");
+			return ((dummy.style.display).indexOf("box") !== -1) ? true : false;
+		},
+		
+		boxModelRenderer : function(params) {
+			var target = params.target,
+			    nodes = target.childNodes,
+			    children = [],
+			    orient = params["box-orient"] || "horizontal",
+			    align = params["box-align"] || "stretch",
+			    direction = params["box-direction"] || "normal",
+			    pack = params["box-pack"] || "start";
 			
-			document.body.appendChild(dummy);
-			return dummy.style.display ? true : false;
+			for (var i = 0, j = nodes.length; i < j; i++) {
+				if (nodes[i].nodeType === 1) {
+					children.push(nodes[i]);
+				}
+			}
+			
+			this.applyBoxModel(target, children);
+			this.applyBoxOrient(target, children, orient);
+			this.applyBoxAlign(target, children, align);
+			this.applyBoxDirection(target, children, direction);
+			this.applyBoxPack(target, children, pack);
 		}
 	};
 	
 	$self.box = function(params) {
-		// console.log(params);
-		// console.log(window.getComputedStyle(params.target, null).getPropertyValue("display"));
 		var support = $self.utils.flexBoxSupported();
 		
 		if (!support) {
-			console.log(support);
+			$self.utils.boxModelRenderer(params);
 		}
 	};
 	
